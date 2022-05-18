79ae064d42d0f7668239de857f2aefa6f02a9bfe
Marian <marianlopig@gmail.com>
2022-05-19 00:51:25 +0200

    unsaved property in serie model

--- db/models/Serie.js
+++ db/models/Serie.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const SerieSchema = new Schema({
   name: String,
-  platforms: String,
+  platform: String,
 });
 
 const Serie = model("Serie", SerieSchema, "series");
