9aa4257f1a4246c7b5940e69560b5ddc968cb1cd
Marian <marianlopig@gmail.com>
2022-05-19 21:00:26 +0200

    Added register in user controllers

---
+++ server/usersControllers.js/usersControllers.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../../db/models/User");
+
+const userLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, password });
+  if (user) {
+    const token = jwt.sign(
+      // eslint-disable-next-line no-underscore-dangle
+      { id: user._id, username: user.username },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "2d" }
+    );
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ msg: "bad request" });
+  }
+};
+
+const userRegister = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user) {
+    const error = new Error();
+    error.statusCode = 409;
+    error.customMessage = "user already exists";
+
+    next(error);
+  }
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const newUser = await User.create({
+      username,
+      password: encryptedPassword,
+    });
+
+    res
+      .status(201)
+      .json({ user: { username: newUser.username, id: newUser.id } });
+  } catch (error) {
+    error.statusCode = 400;
+    error.customMessage = "wront user data";
+
+    next(error);
+  }
+};
+
+module.exports = { userLogin, userRegister };
